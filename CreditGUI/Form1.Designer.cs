ddc2a7b17a2df3f752e01e2e66b29112fe4bc72d
Zack Phoenix <nightphoenix13@hotmail.com>
2014-05-02 14:40:14 -0500

    First Commit. Final assignment turned in.

---
+++ CreditGUI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CreditGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(44, 55);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(105, 13);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = "Enter the credit limit: ";
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Location = new System.Drawing.Point(155, 52);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(50, 20);
+            this.valueTextBox.TabIndex = 1;
+            this.valueTextBox.Text = "0.00";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(98, 87);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(72, 159);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(121, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "The credit limit is $#.##.";
+            this.outputLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.valueTextBox);
+            this.Controls.Add(this.infoLabel);
+            this.Name = "Form1";
+            this.Text = "Credit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ CreditGUI/Form1.cs
@@ -0,0 +1,48 @@
+﻿// Written by Zack Rosales
+// Introduction to C# Programming ITSE 1430 1001
+// Program for page 351, # 9
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreditGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            // local variables
+            double creditLimit;
+
+            // getting input from user
+            creditLimit = Convert.ToDouble(valueTextBox.Text);
+
+            // using method arguments based on user input
+            if (creditLimit == 0.00)
+                CustomerCreditLimit();
+            else
+                CustomerCreditLimit(creditLimit);
+        }
+
+        void CustomerCreditLimit(double limit = 500.00) // CustomerCreditLimit method start
+        {
+            // preparing output labels
+            outputLabel.Text = String.Format("The credit limit is {0}.", limit.ToString("C2"));
+
+            // making output label visible
+            outputLabel.Visible = true;
+        } // CustomerCreditLimit method end
+    }
+}
